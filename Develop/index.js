ce81970ac00f69a41cd6586fdbd5c3591abde51b
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-02-19 14:07:33 -0800

    created prompt userarray, working on index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,77 @@
-// TODO: Include packages needed for this application
+//packages required
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//array of questions for user input
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of the project?"
+        },
+        {
+            type: "input",
+            name: "url",
+            message: "What is the live url for this project?"
+        },
+        {    
+            type: "input",
+            name: "description",
+            message: "What is the description of your project?" 
+        }, 
+        {
+            type: "input",
+            name: "install",
+            message: "What is the installation instructions?"
+        },
+        {
+            type: "input",
+            name: "use",
+            message: "How is this project used?"
+        },
+        {
+            type: "list",
+            message: "Which type of license would you like to use?",
+            name: "license",
+            choices: [
+              "Apache License 2.0", 
+              "MIT License", 
+              "Boost Software License 1.0", 
+              "Eclipse Public License 2.0"
+            ]
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What is your first and last name for copyright license?"
+        },
+        {
+            type: "input",
+            name: "contributions",
+            message: "Please list any contributions made to the project."
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "What is the test instructions for the project?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "Please enter your GitHub username."
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your contact Email?"
+        },
+    ]);
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+//func init to create file
+
+//function call to initialize app
 init();
